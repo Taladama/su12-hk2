c3a24ab53b6307cfc059bff02a2225f0cae391f6
Taladama <xaydagoss@gmail.com>
2026-04-26 09:03:30 +0700

    Add question index

---
+++ data/questions.js
@@ -0,0 +1,28 @@
+export const chapters = [
+  {
+    id: "chapter-5",
+    title: "Chương 5",
+    subtitle: "Lịch sử đối ngoại của Việt Nam thời cận hiện đại"
+  },
+  {
+    id: "chapter-6",
+    title: "Chương 6",
+    subtitle: "Hồ Chí Minh trong lịch sử Việt Nam"
+  }
+];
+
+import { questions1 } from "./question-parts/part-1.js";
+import { questions2 } from "./question-parts/part-2.js";
+import { questions3 } from "./question-parts/part-3.js";
+import { questions4 } from "./question-parts/part-4.js";
+import { questions5 } from "./question-parts/part-5.js";
+import { questions6 } from "./question-parts/part-6.js";
+
+export const questions = [
+  ...questions1,
+  ...questions2,
+  ...questions3,
+  ...questions4,
+  ...questions5,
+  ...questions6
+];
